9766191f72ad9fa9c6c0122603a01baf47112203
pkpepe <aunghtetthu.aht98@gmail.com>
2020-02-06 16:53:24 +0630

    addjhalh

--- app.js
+++ app.js
@@ -178,6 +178,38 @@ function handleMessage(sender_psid, received_message) {
       }
     }
   }
+  }else if (received_message.text == "slide") {
+    response = {
+    "attachment":{
+      "type":"template",
+      "payload":{
+        "template_type":"generic",
+        "elements":[
+           {
+            "title":"Welcome!",
+            "image_url":"https://petersfancybrownhats.com/company_image.png",
+            "subtitle":"We have the right hat for everyone.",
+            "default_action": {
+              "type": "web_url",
+              "url": "https://petersfancybrownhats.com/view?item=103",
+              "webview_height_ratio": "tall",
+            },
+            "buttons":[
+              {
+                "type":"web_url",
+                "url":"https://petersfancybrownhats.com",
+                "title":"View Website"
+              },{
+                "type":"postback",
+                "title":"Start Chatting",
+                "payload":"DEVELOPER_DEFINED_PAYLOAD"
+              }              
+            ]      
+          }
+        ]
+      }
+    }
+  }
   }
   else if (received_message.text == "ni hao") {    
     // Create the payload for a basic text message, which
